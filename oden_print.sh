a3dca460e6a2a0beb529c19857e0d536b89308b6
root <root@TARS>
2022-09-13 21:26:01 -0500

    Added ability to print multiple copies and specify default printer

--- oden_print.sh
+++ oden_print.sh
@@ -1,19 +1,45 @@
-function oden_print {
+unction oden_print {
     # print to an Oden printer wirelessly via SSH to workstation
     # first argument: printer name
     # other arguments: files to be printed 
-    machine=<username>@<machine name>.oden.utexas.edu # users hardcode here
-    if [[ "$1" == "-h" ]]; then
-        echo ''
-        echo 'usage: oden_print <printer> <files to print>'
-        echo 'flags: -h, help'
-        echo '       -l, list printer names'
-        echo ''
-    elif [[ "$1" == "-l" ]]; then
-        ssh ${machine} lpstat -p
-    else
-        printer=$1
-        shift # discard first argument, $2 is now $1
-        ssh ${machine} lpr -P ${printer} < "$@"
-    fi
+    default=<default printer> 
+    machine=<default machine> # users hardcode here
+    copies=1
+
+    case $1 in 
+    #flags for information 	   
+	"-h")     
+        	echo ''
+        	echo 'usage: oden_print <printer> <files to print>'
+        	echo 'If no printer is specified, then default printer is used'
+		echo 'If no number of copies are specified, then default is 1'  
+		echo 'flags: -h, help'
+        	echo '       -l, list printer names'
+		echo ' 	     -d, list default printer'
+		echo '	     -m, list machine used for ssh' 
+		echo '       -#, set number of copies'
+        	echo ''
+    	;; 
+    
+    
+    	"-d") 	echo $default ;; 
+	"-m")	echo $machine ;; 
+	"-l") 	ssh ${machine} lpstat -p ;; 
+	*)	
+		if [[ "$1" == "-#" ]]; then
+			copies=$2
+			shift 
+			shift
+    		fi 	
+		if [[ $# == 1 ]]; then 
+			printer=$default 
+		else 
+			printer=$1
+			shift 
+		fi
+        	ssh ${machine} lpr -P ${printer} -# $copies  < "$@" 
+	;;
+    esac	   
 }
+
+
--- readme.md
+++ readme.md
@@ -6,7 +6,7 @@ A command line script to print wirelessly in the Oden Intitute at UT Austin from
 ## How to set up
 
 - place the contents of `oden_print.sh` in your `.bashrc` or `.profile` file
-- in the copied text replace `<username>` with your sysnet username (for example `jsmith`) and `<machine name>` with your machine's name (for example `bernoulli`)
+- in the copied text replace `<username>` with your sysnet username (for example `jsmith`), `<machine name>` with your machine's name (for example `bernoulli`), and `<printer name>` with your default printer's name
 - re-`source` the file you just edited or start a new terminal session
 - **make sure your personal workstation is always on** (it should be anyway, but the script won't work if it's off)
 
@@ -15,8 +15,12 @@ A command line script to print wirelessly in the Oden Intitute at UT Austin from
 
 - `oden_print -h` : help message
 - `oden_print -l` : list available printers on network
+- `oden_print -d` : list your default printer
+- `oden_print -m` : list your ssh machine 
+- `oden_print -# N printer_name file_to_print` : prints N copies of `file_to_print` on printer `printer_name`
 - `oden_print printer_name file_to_print` : prints `file_to_print` on printer `printer_name`
 
+Note that calling without specifying a printer will print on `<printer name>` 
 
 ## How it works
 
