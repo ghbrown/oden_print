80a4e84799046ddf860753c82ac16a878de4e738
ghbrown <gabriel.h.brown@gmail.com>
2023-02-16 17:24:04 -0600

    Overhaul option handler, add feedthrough to lpr, add (suboptimal) support for multiple files

--- oden_print.sh
+++ oden_print.sh
@@ -1,43 +1,80 @@
 function oden_print {
     # print to an Oden printer wirelessly via SSH to workstation
-    # first argument: printer name
-    # other arguments: files to be printed 
-    machine=<username@machine>        # users hardcode here
-    default_printer=<default printer> # users hardcode here (optional)
-    copies=1
+    machine=username@machine.oden.utexas.edu   # users hardcode here (optional)
+    printer=default_printer                    # users hardcode here (optional)
 
-    case $1 in 
-    #flags for information 	   
-	"-h")     
-        	echo ''
-        	echo 'usage: oden_print <printer> <files to print>'
-        	echo 'If no printer is specified, then default printer is used'
-		    echo 'If no number of copies are specified, then default is 1'  
-		    echo 'flags: -h, help'
-        	echo '       -l, list printer names'
-		    echo ' 	     -d, list default printer'
-		    echo '	     -m, list machine used for ssh' 
-		    echo '       -#, set number of copies'
-        	echo ''
-    	;; 
-    "-d") 	echo ${default_printer} ;; 
-	"-m")	echo ${machine} ;; 
-	"-l") 	ssh ${machine} lpstat -p ;; 
-	*)	
-		if [[ "$1" == "-#" ]]; then
-			copies=$2
-			shift 
-			shift
-    	fi 	
-		if [[ $# == 1 ]]; then 
-			printer=${default_printer}
-		else 
-			printer=$1
-			shift 
-		fi
-        	ssh ${machine} lpr -P ${printer} -# ${copies}  < "$@" 
-	;;
-    esac	   
+    pos_args=    # positional arguments string
+    through=     # flags and options to be passed directly to lpr
+    while [ ${#} -gt 0 ]; do
+        case ${1} in 
+	        -h | --help)     
+        	    echo ''
+        	    echo 'usage: oden_print [options] <files to print>'
+        	    echo 'If no printer or machine is specified, then the defaults are used'
+		        echo 'flags: -h / --help,     help'
+        	    echo '       -d / --defaults, list default printer and machine'
+        	    echo '       -l / --list,     list available printer names'
+		        echo '	     -m / --machine,  specify machine/workstation for ssh' 
+                echo '       -t / --through,  flags and options to be passed through to lpr'
+                echo ''
+                echo 'Example: use machine mandelbrot to print foo.pdf on cp3se'
+                echo '         (two copies, single sided):'
+                echo '  oden_print -m user@mandelbrot.oden.utexas.edu -p cp3se -t "-# 2 -o sides=one-sided" foo.pdf'
+        	    echo ''
+                return 0
+    	        ;; 
+            -d | --show-defaults)
+                echo ''
+                echo "default printer is:             ${printer}"
+                echo "default machine/workstation is: ${machine}"
+                echo ''
+                return 0
+                ;; 
+            -p | --printer)  # set printer
+                printer=${2}
+                shift
+                shift
+                ;;
+	        -m | --machine)  # set machine
+                machine=${2}
+                shift
+                shift
+                ;; 
+	        -l | --list)  # list available printers
+                ssh ${machine} lpstat -p
+                return 0
+                ;; 
+            -t | --through) # multiple arguments to be passed to lpr
+                through=${2}
+                shift
+                shift
+                ;;
+            -* | --*)
+                >&2 echo "oden_print: unknown option ${1}"
+                return 1
+                ;;
+	        *)
+                pos_args+="${1} "  # save positional arguments
+                shift
+                ;;
+            esac
+    done
+
+    if [ -z "${pos_args}" ]; then
+        >&2 echo "oden_print: no files to print"
+        return 1
+    fi
+
+    # Both:
+    #   [usual commands] <(cat ${pos_args})
+    #   cat ${pos_args} | [usual commands]
+    # result in only one file being printed if there is more than one
+    # positional argument (file to print), so using for loop for now.
+    # Could perhaps use scp and tar to place these files in the
+    # remote /tmp directory to avoid multiple credentials checks.
+    for file in ${pos_args}; do
+        ssh ${machine} lpr -P ${printer} ${through} < ${file}
+    done
 }
 
 
--- readme.md
+++ readme.md
@@ -2,25 +2,31 @@
 
 A command line script to print wirelessly in the Oden Intitute at UT Austin from Linux, Mac, or Windows (if you're running Windows Subsystem for Linux).
 
+## Set up
 
-## How to set up
+Clone this repository, then place the contents of `oden_print.sh` in your `.bashrc` or `.profile` file and re-`source` that file or start a new terminal session.
 
-- place the contents of `oden_print.sh` in your `.bashrc` or `.profile` file
-- in the copied text, replace `<username@machine>` with your sysnet username and machine (for example `jsmith@bernoulli.oden.utexas.edu`), replace `<default printer>` with your default printer's name if desired
-- re-`source` the file you just edited or start a new terminal session
-- **make sure your personal workstation is always on** (it should be anyway, but the script won't work if it's off)
+**Make sure your personal workstation is always on** (it should be anyway, but the script won't work if it's off).
+
+**Recommended**: This will ensure you don't have to specify a workstation and printer for every print:
+- in the copied text, replace `username@machine.oden.utexas.edu` with your sysnet username and machine (for example `jsmith@bernoulli.oden.utexas.edu`)
+- in the copied text, replace `default_printer` with your default printer's name
 
 
 ## How to use
 
-- `oden_print -h` : help
-- `oden_print -l` : list printer names
-- `oden_print -d` : list default printer
-- `oden_print -m` : list machine used for ssh
-- `oden_print printer_name file_to_print` : prints `file_to_print` on printer `printer_name`
-- `oden_print -# N printer_name file_to_print` : prints `N` copies of `file_to_print` on printer `printer_name`
+Typical usage for users who have done minimal and maximal set up are
+```bash
+# minimal
+oden_print -m username@machine.oden.utexas.edu -p cp3se file.pdf
+# maximal
+oden_print file.pdf
+```
 
-Calling without specifying a printer will print on the default printer set in the body of the function. 
+To get a list of available printers and their name use `oden_print -l`.
+
+Use `oden_print -h` for a full help message.
+
 
 ## How it works
 
@@ -30,8 +36,7 @@ It's basically a one-liner (with some wrappings for convenience and usability).
 
 ## Miscellaneous
 
-Without any SSH configurations, you'll have to enter your remote machine password every time you print.
+Without any SSH configurations, you'll have to enter your remote machine password for every file you print.
 To avoid this, [set up an SSH keypair](https://www.oden.utexas.edu/sysdocs/ssh/index.html).
 
 **If you have any trouble with the script or have any improvements let me know.**
-Improvements for print configuratins (double sided, etc.) coming soon.
